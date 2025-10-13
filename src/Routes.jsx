1b6dfccca59adfdbf8e45d67df25af3d34c072eb
fmnihal <fattahmahmudnihal@gmail.com>
2025-10-14 00:51:28 +0600

    init 3

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import Root from "./components/Root";
 import Home from "./components/Home";
+import Apps from "./components/Apps";
+import Installation from "./components/Installation";
 
 export const router = createBrowserRouter([
     {
@@ -10,7 +12,15 @@ export const router = createBrowserRouter([
             {
                 index: true,
                 element:<Home></Home>,
-            }
+            },
+            {
+                path: "apps", 
+                element: <Apps />,
+            },
+            {
+                path: "installation",
+                element: <Installation />,
+            },
         ]
     },
 ]);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import logoImg from '../assets/logo.png'
-
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
-        <div className='w-full py-4'>
-            {/* <h1>Header</h1> */}
-            <div className='w-11/12 flex justify-around items-center'>
-                <div className='flex items-center gap-2'>
-                    <img src={logoImg} alt="" className='h-10' />
-                    <h1 className='text-purple'>HERO.IO</h1>
-                </div>
-                <ul className='flex gap-4'>
-                    <li>Home</li>
-                    <li>Apps</li>
-                    <li>Installation</li>
-                </ul>
-                <button className='bg-purple text-white'>Contribute</button>
+        // <div className='w-full py-4'>
+        //     {/* <h1>Header</h1> */}
+        //     <div className='w-11/12 flex justify-around items-center'>
+        //         <div className='flex items-center gap-2'>
+        //             <img src={logoImg} alt="" className='h-10' />
+        //             <h1 className='text-purple'>HERO.IO</h1>
+        //         </div>
+        //         <ul className='flex gap-4'>
+        //             <li>Home</li>
+        //             <li>Apps</li>
+        //             <li>Installation</li>
+        //         </ul>
+        //         <button className='bg-purple text-white'>Contribute</button>
+        //     </div>
+        // </div>
+
+        <header className="navbar bg-base-100">
+            <div className="flex-1">
+                {/* Link to the Home page (which is the index/default route) */}
+                <Link to="/" className="btn btn-ghost text-xl">
+                    AppDroid
+                </Link>
             </div>
-        </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        {/* Link to the Apps page: /apps */}
+                        <Link to="/apps">Apps</Link> 
+                    </li>
+                    <li>
+                        {/* Link to the Installation page: /installation */}
+                        <Link to="/installation">Installation</Link> 
+                    </li>
+                    {/* The Home page is the default, but you can also explicitly link to it */}
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                </ul>
+            </div>
+        </header>
     );
 };
 
